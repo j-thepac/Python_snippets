6ae0d2a632f3d6e9e36d286ee725d1ae7b090ce8
Deepak <j.thepac@gmail.com>
2021-02-26 19:52:14 +0530

    added new code

---
+++ in_list.py
@@ -0,0 +1,5 @@
+def include(arr,item):
+    if item in arr: return True
+    else: return False
+    
+assert (include([1,2,3,4], 3)== False )
---
+++ odd_r_even.py
@@ -0,0 +1,7 @@
+def odd_or_even(arr):
+    if(sum(arr)%2==0):return "even" 
+    else:return "odd"
+
+
+
+assert (odd_or_even([0, 1, 2])== "odd")
