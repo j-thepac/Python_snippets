0537d8e95cd8cdf1830336894e2081bf09a0d1db
Deepak Jayaprakash <j.thepac@gmail.com>
2022-02-26 03:59:19 +0000

    dotcat

---
+++ DotCal/dotcal.py
@@ -0,0 +1,35 @@
+
+"""
+calculator("..... + ...............") => "...................."
+calculator("..... - ...") => ".."
+calculator("..... - .") => "...."
+calculator("..... * ...") => "..............."
+calculator("..... * ..") => ".........."
+calculator("..... // ..") => ".."
+calculator("..... // .") => "....."
+calculator(". // ..") => ""
+calculator(".. - ..") => ""
+
+docker run -it -v $PWD:/home/apps -w /home/apps -p 5000:5000 python:3.9-slim python dotcal.py
+"""
+def calculator(txt): 
+    l1,sign,l2=txt.split(" ") 
+    v="{}{}{}".format(len(l1),sign,len(l2)) 
+    return (eval(v)*".")
+
+def calc(txt):
+    l1,sign,l2=txt.split(" ")
+    v=str(len(l1))+sign+str(len(l2))
+    print(eval(v)*".")
+    return (eval(v)*".")
+
+
+assert(calc("..... + ...............")== "....................")
+assert(calc("..... - ...")== "..")
+assert(calc("..... - .")== "....")
+assert(calc("..... * ...")== "...............")
+assert(calc("..... * ..")== "..........")
+assert(calc("..... // ..")== "..")
+assert(calc("..... // ."), ".....")
+assert(calc(". // .."), "")
+assert(calc(". - ."), "")
