e51d776193700839dc029d7c8124976f99ee3740
Deepak Jayaprakash <j.thepac@gmail.com>
2022-09-24 03:12:44 +0000

    working code

--- NameNo/nameno.py
+++ NameNo/nameno.py
@@ -8,12 +8,20 @@ Words should be separated by only spaces and not hyphens. No need to validate pa
 """
 def name_that_number(x):
     n1={0:"zero",1:"one",2:"two",3:"three",4:"four",5:"five",6:"six",7:"seven",8:"eight",9:"nine",10:"ten"}
-    n2={11:"eleven",12:"twelve",13:"thirteen",14:"fourteen",15:"fifteen",16:"sixteen",17:"seventeen",18:"eighteen",19:"ninteen"}
-    n3={20:"twenty",30:"thirty",40:"forty",50:"fifty",60:"sixty",70:"seventy",80:"eighty",90:"ninty"}
+    n2={11:"eleven",12:"twelve",13:"thirteen",14:"fourteen",15:"fifteen",16:"sixteen",17:"seventeen",18:"eighteen",19:"nineteen"}
+    n3={20:"twenty",30:"thirty",40:"forty",50:"fifty",60:"sixty",70:"seventy",80:"eighty",90:"ninety"}
     if(x<=10):return n1[x]
+    elif(x<=19):return n2[x]
+    elif(x%10==0):return n3[x]
+    else:
+        rem=x%10
+        div=(x//10)*10
+        return (f"{n3[div]} {n1[rem]}")
+
 
 
 assert(name_that_number(0)== 'zero')
 assert(name_that_number(4)== 'four')
 assert(name_that_number(9)== 'nine')
 assert(name_that_number(23)== 'twenty three')
+print("done")
