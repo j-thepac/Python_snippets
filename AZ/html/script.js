078aedc96b3c800dba199c903ce5355cbdc42ace
Deepak <j.thepac@gmail.com>
2024-03-10 15:13:14 +0530

    AZ

---
+++ AZ/html/script.js
@@ -0,0 +1,12 @@
+async function fn(){
+    data=document.getElementById("a").value
+    payload={
+        method:"POST"
+        ,headers:{"Content-Type":"application/json"}
+        ,body:JSON.stringify({"data":data})
+    }
+
+    resp=await fetch("http://0.0.0.0:5000/",payload)
+    result= await resp.text()
+    document.getElementById("b").value=result
+}
